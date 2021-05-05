6e17e495e5a8b0d04fa956a460747c9c2dab87a4
Brandi Flynn <pvtpyro@yahoo.com>
2021-05-04 22:13:23 -0700

    add bootstrap

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+//set DEBUG=* & node app.js  using CMD only
 var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
 
 var app = express();
 
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+
 app.get('/', function (req, res) {
-	res.send('Hello from my books app');
+	res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
 app.listen(3000, function () {
-	console.log('listening on port 3000');
+	debug(`listening on port ${chalk.greenBright('3000')}`); 
 });
